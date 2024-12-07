5d3aa37950e7340f6a8d544ff230338024d48d8d
DaVinchik2 <yoz.poco@gmail.com>
2024-12-07 12:49:37 +0300

    Update songs.ts

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -3,57 +3,24 @@ import { Song } from '../types/music';
 export const songs: Song[] = [
   {
     id: '1',
-    title: 'Midnight Piano',
-    artist: 'Georg',
-    genre: 'nostalgic acoustic folk',
+    title: 'Hot Room',
+    artist: 'AI Creator',
+    genre: 'Rock opera, rock ballad',
     coverUrl: 'https://cdn2.suno.ai/bcc7137c-4b55-404d-a2dc-baacebab0d4d_63985270.jpeg',
     audioUrl: 'https://cdn1.suno.ai/9574d25d-b11e-4f4d-a2c1-435fdc6299af.mp3',
     duration: 120,
     plays: 11014,
-    likes: 9
+    likes: 1547
   },
   {
     id: '2',
-    title: 'Urban Nights',
-    artist: 'Rebecca',
-    genre: 'experimental, Neo-Soul',
+    title: 'Песня про пиво',
+    artist: 'AI Creator',
+    genre: 'dance-pop, synthwave / electro, synth-pop, electropop, female vocalist',
     coverUrl: 'https://images.unsplash.com/photo-1555661059-7e755c1c3c1d?w=400&h=400&fit=crop',
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/05/27/audio_1808fbf07a.mp3',
-    duration: 197,
-    plays: 0,
-    likes: 0
-  },
-  {
-    id: '3',
-    title: 'Neon Dreams',
-    artist: 'ClaraCo',
-    genre: 'electronic, jazz',
-    coverUrl: 'https://images.unsplash.com/photo-1557672172-298e090bd0f1?w=400&h=400&fit=crop',
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_946f4a8889.mp3',
-    duration: 239,
-    plays: 0,
-    likes: 0
-  },
-  {
-    id: '4',
-    title: 'Test Test',
-    artist: 'CoCoCo',
-    genre: 'electronic, jazz',
-    coverUrl: 'https://images.unsplash.com/photo-1557672172-298e090bd0f1?w=400&h=400&fit=crop',
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_946f4a8889.mp3',
-    duration: 239,
-    plays: 0,
-    likes: 0
-  },
-  {
-    id: '5',
-    title: 'Tatatatat',
-    artist: 'OSSSEEEE',
-    genre: 'electronic, jazz',
-    coverUrl: 'https://images.unsplash.com/photo-1557672172-298e090bd0f1?w=400&h=400&fit=crop',
-    audioUrl: 'https://cdn.pixabay.com/download/audio/2022/10/25/audio_946f4a8889.mp3',
-    duration: 239,
-    plays: 0,
-    likes: 0
+    audioUrl: 'https://cdn1.suno.ai/1be173ab-8a22-40f1-a481-512fb7e109ed.mp3',
+    duration: 183,
+    plays: 13578,
+    likes: 2215
   }
 ];
